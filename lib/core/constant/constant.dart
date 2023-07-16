4a6cdabe9a5522c8fdbb51ab2078ea8aa73d3afa
MehnArkar <mehnnarkar@gmail.com>
2023-07-16 16:00:20 +0630

    repository and usecases

---
+++ lib/core/constant/constant.dart
@@ -0,0 +1,6 @@
+
+
+const String newsAPIBaseURL = 'https://newsapi.org/v2';
+const String apiKey = '080d1db7b636482a9826cb1faf388d90';
+const String countryQurey = 'us';
+const String categoryQuery = 'general';
--- lib/features/daily_news/data/repository/article_repository_impl.dart
+++ lib/features/daily_news/data/repository/article_repository_impl.dart
@@ -1,13 +1,38 @@
+import 'dart:io';
+
+import 'package:flutter_clean_architecture/core/constant/constant.dart';
 import 'package:flutter_clean_architecture/core/resources/data_state.dart';
+import 'package:flutter_clean_architecture/features/daily_news/data/data_source/remote/news_api_server.dart';
 import 'package:flutter_clean_architecture/features/daily_news/data/models/article.dart';
 import 'package:flutter_clean_architecture/features/daily_news/domain/entities/article.dart';
 import 'package:flutter_clean_architecture/features/daily_news/domain/respository/article_repository.dart';
+import 'package:dio/dio.dart';
 
 class ArticleRepositoryImpl implements ArticleRepository{
+  final NewsApiService newsApiService;
+  ArticleRepositoryImpl(this.newsApiService);
+
+
   @override
-  Future<DataState<List<ArticleModel>>> getNewsArticles() {
-    // TODO: implement getNewsArticles
-    throw UnimplementedError();
+  Future<DataState<List<ArticleModel>>> getNewsArticles() async {
+    try {
+      final response = await newsApiService.getNewsArticles(
+          apiKey: apiKey,
+          country: countryQurey,
+          category: categoryQuery
+      );
+
+      if (response.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(response.data);
+      } else {
+        return DataFailed(DioError(error: response.response.statusMessage,
+            response: response.response,
+            type: DioErrorType.badResponse,
+            requestOptions: response.response.requestOptions));
+      }
+    } on DioError catch(e){
+      return DataFailed(e);
+    }
   }
 
 }
